879123b3cbaf73a74c359a4c86a88e71312d42dd
chandhru-u <chandhrukrish06@gmail.com>
2025-11-19 23:35:58 +0530

    Update final.py

--- final.py
+++ final.py
@@ -7,7 +7,7 @@ st.markdown("""
 
     /* Main background: very light gray */
     .stApp {
-        background-color: #f5f7fa !important; 
+        background-color: #fafafa !important; 
     }
 
     /* Sidebar beautiful gradient navy + blue */
@@ -154,6 +154,7 @@ st.line_chart(user_data.set_index('timestamp')['energy_consumed'])
 peak_usage_time = user_data[user_data['energy_consumed'] == user_data['energy_consumed'].max()]
 st.write(f"**Peak Usage Time**: {peak_usage_time['timestamp'].dt.strftime('%Y-%m-%d %H:%M:%S').values[0]}")
 st.write(f"**Total Energy Consumed**: {user_data['energy_consumed'].sum()} kWh")
+
 
 
 
